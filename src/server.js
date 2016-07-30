0ba39865e031bcef5b6d8121935e596207a2d2be
rahuldean <godhc@me.com>
2016-07-30 20:41:00 +0000

    added html content for the site

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
-var express = require("express");
-var app = express();
+var express = require("express"),
+    app = express(),
+    compression = require('compression');
 
 var port = process.env.PORT || 8080;
 var host = process.env.IP || "localhost";
+var oneDay = 86400000;
 
-app.get('/', function(req, res){
-    res.json({});
-});
+app.use(compression());
+app.use(express.static('public'));
+app.use('/static', express.static(__dirname + '/public', { maxAge: oneDay }));
 
 app.listen(port, host, function(){
     console.log("App running @host: " + host + " @port:" + port);
