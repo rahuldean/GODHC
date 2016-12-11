6907c7334292bb8888c55c8442e107705afb095a
Rahul Reddy Malkireddy <rahuldean@gmail.com>
2016-12-11 15:08:38 +0000

    rendering index.html for root requests

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     app = express(),
-    compression = require('compression');
+    compression = require('compression'),
+    path = require('path');
 
 var port = process.env.PORT || 8080;
 var host = process.env.IP || "localhost";
@@ -9,6 +10,10 @@ var oneDay = 86400000;
 app.use(compression());
 app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname + '/public' + '/index.html'));
+});
 
 app.listen(port, host, function(){
     console.log("App running @host: " + host + " @port:" + port);
